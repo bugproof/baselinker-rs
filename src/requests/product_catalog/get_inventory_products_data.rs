cd4e84ca2193758cc37f9ae6240b0f3f654174c5
Konrad <3116731+bugproof@users.noreply.github.com>
2022-05-19 12:46:27 +0200

    GetInventoryProductsData

--- src/requests/product_catalog/get_inventory_products_data.rs
+++ src/requests/product_catalog/get_inventory_products_data.rs
@@ -1,1 +1,38 @@
-// TODO
+use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+use crate::common::RequestTrait;
+use crate::requests::product_catalog::add_inventory_product::ProductLink;
+
+#[derive(Serialize, Deserialize)]
+pub struct DetailedProductData {
+    pub ean: String,
+    pub sku: String,
+    pub tax_rate: f64,
+    pub weight: f64,
+    pub height: f64,
+    pub width: f64,
+    pub length: f64,
+    pub star: i64,
+    pub category_id: i64,
+    pub manufacturer_id: i64,
+    pub prices: HashMap<i64, f64>,
+    pub stock: HashMap<String, i64>,
+    pub locations: HashMap<String, String>,
+    pub text_fields: HashMap<String, String>,
+    pub images: Vec<String>,
+    pub links: HashMap<String, ProductLink>,
+    pub variants: HashMap<String, serde_json::Value>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct GetInventoryProductsDataResponse {
+    pub products: HashMap<String, DetailedProductData>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct GetInventoryProductsData {
+    pub inventory_id: i64,
+    pub products: Vec<String>,
+}
+
+impl RequestTrait<GetInventoryProductsDataResponse> for GetInventoryProductsData { const METHOD: &'static str = "getInventoryProductsData"; }
